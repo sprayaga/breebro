247e675a44a728bfe8af077cc78044c45dc18544
sprayaga <sprayagaa@gmail.com>
2021-07-14 11:55:25 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+function setup(){
+    canvas=createCanvas(420,250);
+    canvas.center();
+    video=createCapture(VIDEO);
+    video.hide();
+    classifier=ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/Af-BIxSs8/model.json',modelLoaded);
+}
+
+function draw(){
+    image(video,0,0,420,250);
+   classifier.classify(video,gotResults);
+}
+
+function modelLoaded(){
+    console.log('model is loaded');
+}
+
+function gotResults(error,results){
+    if(error){
+    console.error(error);
+    }
+    else{
+        console.log(results);
+        document.getElementById('j').innerHTML=' : '+results[0].label;
+        document.getElementById('k').innerHTML=' : '+results[0].confidence.toFixed(3);
+    }
+}
